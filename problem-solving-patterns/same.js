125df159979dbdcbe6ce7440dab6a144856f6bb3
gustcustodio <gustcustodio@gmail.com>
2024-12-12 09:00:43 -0300

    update same.js

--- problem-solving-patterns/same.js
+++ problem-solving-patterns/same.js
@@ -4,23 +4,23 @@
 
 // * a naive solution - time complexity: O(N^2) * //
 
-// function same(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-//   for (let i = 0; i < arr1.length; i++) {
-//     let correctIndex = arr2.indexOf(arr1[i] ** 2);
-//     if (correctIndex === -1) {
-//       return false;
-//     }
-//     console.log(arr2);
-//     arr2.splice(correctIndex, 1);
-//   }
-//   return true;
-// }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
 
-// console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
 
 // * refactored - time complexity: O(N) * //
 
